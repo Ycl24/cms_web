26ffbe615d2ade74f61133927df516102f5fdb4a
v_chenlyu <v_chenlyu@tencent.com>
2020-05-14 17:28:00 +0800

    更新

--- script.sh
+++ script.sh
@@ -1,17 +1,64 @@
 #分支名称
 branch_name='master'
+
+#镜像前缀名称
+images_name='cms_web'
+
+#dockerfile path  相对路径
+dockerfile_path='Dockerfile'
+
+#project path 项目路径 相对路径
+project_path='/data/webroot/cms_web'
+
+#webroot path 项目路径 相对路径
+webroot_path='/data/webroot'
+
+#script path 脚本 相对路径
+script_path='/data/webroot/cms_web_script'
+
+
 #删除文件夹
-rm -rf cms_web
+rm -rf ${project_path}
+
+cd ${webroot_path}
 #不存在-拉去git项目
 git clone https://github.com/Ycl24/cms_web.git
 
-chmod -R 777 cms_web
+chmod -R 777 ${project_path}
 
-cd cms_web
+cd ${project_path}
 
 git checkout ${branch_name}
 
 git pull origin ${branch_name}
 
-#删除docker 80 crm_web dist镜像
+#查询镜像
+images_name_info=`docker images ${images_name}`
 
+#判断镜像是否存在
+strop_images_name= `echo ${images_name_info} | grep "${images_name}" `
+if [[ "$result" != "" ]]
+ then
+    #镜像存在
+    #查询所有的容器，过滤出Exited状态的容器，列出容器ID，删除这些容器
+    docker rm  -f `docker ps -a|grep ${images_name}|awk '{print $1}'`
+
+    #删除docker crm_web 关键字的镜像
+    docker rmi -f `docker images | grep ${images_name} | awk '{print $3}'` 
+ fi
+
+
+
+
+#获取git项目最后的tag
+tag_name=`git describe --tags \`git rev-list --tags --max-count=1\``
+
+cd ${script_path}
+
+#dockerfile打包生成镜像
+docker build -f ${dockerfile_path} -t ${images_name}:${tag_name}  ${project_path}
+
+#清理所有悬挂（<none>）镜像
+docker image prune
+
+y
